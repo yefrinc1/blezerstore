6ac838ecc0f685b74d6a91cd1ad7592a4c56aa7b
Yefrin Castano Sanchez <yefrin.castano@adhesiva.local>
2023-05-29 10:58:04 -0500

    Organizar Links

--- assets/js/main.js
+++ assets/js/main.js
@@ -33,31 +33,6 @@ document.addEventListener('DOMContentLoaded', () => {
   }
 
   /**
-   * Navbar links active state on scroll
-   */
-  let navbarlinks = document.querySelectorAll('#navbar a');
-
-  function navbarlinksActive() {
-    navbarlinks.forEach(navbarlink => {
-
-      if (!navbarlink.hash) return;
-
-      let section = document.querySelector(navbarlink.hash);
-      if (!section) return;
-
-      let position = window.scrollY + 200;
-
-      if (position >= section.offsetTop && position <= (section.offsetTop + section.offsetHeight)) {
-        navbarlink.classList.add('active');
-      } else {
-        navbarlink.classList.remove('active');
-      }
-    })
-  }
-  window.addEventListener('load', navbarlinksActive);
-  document.addEventListener('scroll', navbarlinksActive);
-
-  /**
    * Mobile nav toggle
    */
   const mobileNavShow = document.querySelector('.mobile-nav-show');
